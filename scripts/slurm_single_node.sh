293ee58d9c82304ca9b762251f1d74574a851497
Philipp Samfass <samfass@in.tum.de>
2022-07-11 17:38:29 +0200

    Update slurm_single_node.sh

--- scripts/slurm_single_node.sh
+++ scripts/slurm_single_node.sh
@@ -22,6 +22,9 @@ module load openblas
 module load mpi/ompi-4.1.x
 
 hpl_runscript=./run_xhplhip.sh
+
+cp ../scripts/config/HPL_1GPU.dat HPL.dat
+
 filename=HPL.dat
 
 P=$(sed -n "11, 1p" ${filename} | awk '{print $1}')
@@ -34,5 +37,6 @@ total_cpu_cores=$(($num_cpu_cores*$num_cpu_sockets))
 
 mpi_args="--map-by slot:PE=${total_cpu_cores} --bind-to core:overload-allowed --mca btl ^openib --mca pml ucx --report-bindings -x LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm/lib ${mpi_args}"
 
+export AMD_LOG_LEVEL=1
 # For hybrid MPI+OpenMP jobs, thread affinity settings should be configured (e.g., via OMP_PLACES, OMP_PROC_BIND) 
 mpirun -n ${np} ${mpi_args} ${hpl_runscript} # choose appropriate affinity settings for MPI here
